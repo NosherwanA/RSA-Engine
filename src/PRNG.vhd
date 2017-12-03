cd5b100d172dba78f98ce802da2970c3876f5ce5
NosherwanA <nosherwan.a2@gmail.com>
2017-12-03 14:32:52 -0500

    Modified PRNG to give 8 bit number

--- src/PRNG.vhd
+++ src/PRNG.vhd
@@ -1,23 +1,22 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use work.lfsr_pkg.all;
 
-entity prng_10_bit is port(
+entity PRNG is port(
 
 	clk		: in std_logic;
 	rst		: in std_logic;
 	enable	: in std_logic;
-	output	: out std_logic_vector(9 downto 0)
+	output	: out std_logic_vector(7 downto 0)
 
 );
-end prng_10_bit;
+end PRNG;
 
-architecture internal of prng_10_bit is
+architecture internal of PRNG is
 	
 	component lfsr_bit is port (
 
 		reset		: in std_logic;
-		clk		: in std_logic;
+		clk			: in std_logic;
 		en			: in std_logic;
 		seed		: in std_logic_vector(9 downto 0);
 		rbit_out	: out std_logic
@@ -26,10 +25,8 @@ architecture internal of prng_10_bit is
 	end component;
 	
 	--From Random.org
-	
-	signal seed0:		std_logic_vector(9 downto 0):= "0000111010";
+
 	signal seed1:		std_logic_vector(9 downto 0):= "1111011100";
-	signal seed2:		std_logic_vector(9 downto 0):= "0100100101";
 	signal seed3:		std_logic_vector(9 downto 0):= "0110110101";
 	signal seed4:		std_logic_vector(9 downto 0):= "0101110111";
 	signal seed5:		std_logic_vector(9 downto 0):= "1001110110";
@@ -42,8 +39,6 @@ architecture internal of prng_10_bit is
 
 begin
 	
-	lfsr0	: lfsr_bit port map( rst, clk, enable, seed0, output(9));
-	lfsr1	: lfsr_bit port map( rst, clk, enable, seed2, output(8));
 	lfsr2	: lfsr_bit port map( rst, clk, enable, seed7, output(7));
 	lfsr3	: lfsr_bit port map( rst, clk, enable, seed1, output(6));
 	lfsr4	: lfsr_bit port map( rst, clk, enable, seed9, output(5));
