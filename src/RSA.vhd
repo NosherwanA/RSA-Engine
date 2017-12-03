8c9cd6ddc7305fdb93b9791e400e03095e0b2dd1
NosherwanA <nosherwan.a2@gmail.com>
2017-12-03 15:30:12 -0500

    Added internal components for RSA

--- src/RSA.vhd
+++ src/RSA.vhd
@@ -13,11 +13,49 @@ entity RSA is
         message             : in std_logic_vector(7 downto 0);
         key                 : in std_logic_vector(7 downto 0);
         modulus             : in std_logic_vector(7 downto 0);
-        result              : out std_logic_vector(7 downto 0);
+        result              : out std_logic_vector(7 downto 0)
     );
 end RSA;
 
 architecture internal of RSA is
+
+    -- COMPONENTS USED
+    component Modular_Exponentiator is
+        port(
+            base            : in std_logic_vector(7 downto 0);
+            exponent        : in std_logic_vector(7 downto 0);
+            modulus         : in std_logic_vector(7 downto 0);
+            clk             : in std_logic;
+            reset           : in std_logic;
+            start           : in std_logic;
+            busy            : out std_logic;
+            result          : out std_logic_vector(7 downto 0);
+            done            : out std_logic
+        );
+    end component;
+
+    component MRT is
+        port(
+            numberToCheck               : in std_logic_vector (7 downto 0);
+            clk                         : in std_logic;
+            reset                       : in std_logic;
+            start                       : in std_logic;
+            isPrime                     : out std_logic;
+            busy                        : out std_logic;
+            done                        : out std_logic
+        );
+    end component;
+
+    component PRNG is 
+        port(
+            clk		: in std_logic;
+            rst		: in std_logic;
+            enable	: in std_logic;
+            output	: out std_logic_vector(7 downto 0)
+        );
+    end component;
+
+    -- SIGNALS USED
 
 begin
 
