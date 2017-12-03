7546e414f2cfe7fcdbf757826776d7eae3a2e9a8
NosherwanA <nosherwan.a2@gmail.com>
2017-12-03 15:25:41 -0500

    Added the input output ports for RSA

--- src/RSA.vhd
+++ src/RSA.vhd
@@ -4,7 +4,16 @@ use ieee.numeric_std.all;
 
 entity RSA is
     port(
-
+        clk                 : in std_logic;
+        reset               : in std_logic;
+        start               : in std_logic;
+        busy                : out std_logic;
+        done                : out std_logic;
+        instruction         : in std_logic_vector(1 downto 0);
+        message             : in std_logic_vector(7 downto 0);
+        key                 : in std_logic_vector(7 downto 0);
+        modulus             : in std_logic_vector(7 downto 0);
+        result              : out std_logic_vector(7 downto 0);
     );
 end RSA;
 
